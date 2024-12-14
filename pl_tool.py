f0bffbfb18e46fea1979b520650ec9508d397dc8
sonwe1e <sonwe1e@outlook.com>
2024-12-14 23:00:44 +0800

    重构LightningModule以支持对抗训练，添加生成器和判别器模型，更新训练和验证步骤，计算PSNR和SSIM指标

--- pl_tool.py
+++ pl_tool.py
@@ -1,52 +1,49 @@
 import torch
-import seaborn as sns
-import matplotlib.pyplot as plt
 import lightning.pytorch as pl
-from torchmetrics.segmentation import MeanIoU, GeneralizedDiceScore
+import torch.nn.functional as F
+import numpy as np
+import torchmetrics as tm
+from torch.optim import AdamW
+import torchvision
 
 torch.set_float32_matmul_precision("high")
 
 
 class LightningModule(pl.LightningModule):
+    """
+    使用对抗训练的 Lightning 模块，用于图像超分辨率任务。
+
+    包含一个生成器模型和一个判别器模型。
+    """
+
     def __init__(self, opt, model, len_trainloader):
         """
-        初始化 LightningModule。
+        初始化 Lightning 模块。
 
         Args:
             opt: 包含训练参数的配置对象。
-            model: 要训练的 PyTorch 模型。
+            model: 生成器模型。
             len_trainloader: 训练数据加载器的长度，用于学习率调度器。
         """
         super().__init__()
         self.learning_rate = opt.learning_rate
         self.len_trainloader = len_trainloader
         self.opt = opt
-        self.model = model
-        self.ce_loss = torch.nn.CrossEntropyLoss()  # 交叉熵损失函数
-        # 用于存储训练和验证阶段的预测值和真实标签，在epoch结束时计算指标
-        self.train_pred = []
-        self.train_label = []
-        self.valid_pred = []
-        self.valid_label = []
-        # 初始化训练和验证集的指标计算器
-        self.train_iou = MeanIoU(num_classes=opt.num_classes, per_class=True)
-        self.train_dice = GeneralizedDiceScore(
-            num_classes=opt.num_classes, per_class=True
-        )
-        self.valid_iou = MeanIoU(num_classes=opt.num_classes, per_class=True)
-        self.valid_dice = GeneralizedDiceScore(
-            num_classes=opt.num_classes, per_class=True
-        )
+        self.model = model  # 生成器模型
+        self.DNet = torchvision.models.resnet18(num_classes=1)  # 判别器模型
+        self.l1loss = torch.nn.SmoothL1Loss()  # L1平滑损失
+        self.adversarial_loss = torch.nn.BCEWithLogitsLoss()  # 二元交叉熵损失
+        self.automatic_optimization = False  # 关闭自动优化，使用手动优化
 
     def forward(self, x):
         """
-        模型的前向传播。
+        生成器模型的前向传播。
 
         Args:
             x: 输入张量。
 
         Returns:
-            模型的预测输出。
+            生成器模型的预测输出。
         """
         pred = self.model(x)
         return pred
@@ -56,29 +53,35 @@ class LightningModule(pl.LightningModule):
         配置优化器和学习率调度器。
 
         Returns:
-            包含优化器和学习率调度器的字典。
+            包含生成器和判别器优化器和学习率调度器的字典。
         """
-        self.optimizer = torch.optim.AdamW(
-            self.parameters(),
+        self.optimizer1 = AdamW(  # 生成器优化器
+            self.model.parameters(),
             weight_decay=self.opt.weight_decay,
-            betas=(0.9, 0.95),
             lr=self.learning_rate,
         )
-        self.scheduler = torch.optim.lr_scheduler.OneCycleLR(
-            self.optimizer,
+        self.optimizer2 = AdamW(  # 判别器优化器
+            self.DNet.parameters(),
+            weight_decay=self.opt.weight_decay,
+            lr=self.learning_rate,
+        )
+        self.scheduler = torch.optim.lr_scheduler.OneCycleLR(  # 生成器学习率调度器
+            self.optimizer1,
             max_lr=self.learning_rate,
             epochs=self.opt.epochs,
-            pct_start=0.06,
+            pct_start=0.015,
             steps_per_epoch=self.len_trainloader,
-            anneal_strategy="linear",
         )
-        return {
-            "optimizer": self.optimizer,
-            "lr_scheduler": {
-                "scheduler": self.scheduler,
-                "interval": "step",
+        return (
+            {
+                "optimizer": self.optimizer1,
+                "lr_scheduler": {
+                    "scheduler": self.scheduler,
+                    "interval": "step",
+                },
             },
-        }
+            {"optimizer": self.optimizer2},
+        )
 
     def training_step(self, batch, batch_idx):
         """
@@ -87,77 +90,105 @@ class LightningModule(pl.LightningModule):
         Args:
             batch: 一个批次的训练数据。
             batch_idx: 批次的索引。
-
-        Returns:
-            训练损失。
         """
-        image, label = (batch["image"], batch["label"])
-        prediction = self(image)
-        ce_loss = self.ce_loss(prediction, label)  # 计算交叉熵损失
-        loss = ce_loss  # 总损失等于交叉熵损失
-        self.train_pred.append(prediction)  # 存储预测值
-        self.train_label.append(label)  # 存储真实标签
-        self.log("train_ce_loss", ce_loss)  # 记录训练损失
-        self.log("train_loss", loss)  # 记录总损失
+        x, y = batch  # x 是低分辨率图像，y 是高分辨率图像
+        optimizer_g, optimizer_d = self.optimizers()  # 获取生成器和判别器优化器
+        scheduler = self.scheduler  # 获取学习率调度器
+
+        # 训练生成器
+        self.toggle_optimizer(optimizer_g)  # 激活生成器优化器
+        pred = self.model(x)  # 生成器生成超分辨率图像
+        pred = torch.clamp(pred, -1, 1)  # 将生成图像的像素值限制在 -1 到 1 之间
+        valid = torch.ones(x.size(0), 1).type_as(x)  # 创建真标签，用于判别器
+        l1loss = self.l1loss(pred, y)  # 计算生成图像和真实图像的L1损失
+        g_loss = self.adversarial_loss(self.DNet(pred), valid)  # 计算生成器对抗损失
+        loss = l1loss + 10 * self.scheduler.get_last_lr()[0] * (g_loss)  # 总生成器损失
+
+        self.manual_backward(loss)  # 手动计算梯度
+        optimizer_g.step()  # 更新生成器参数
+        scheduler.step()  # 更新学习率
+        optimizer_g.zero_grad()  # 清空生成器梯度
+        self.untoggle_optimizer(optimizer_g)  # 关闭生成器优化器
+
+        # 训练判别器
+        self.toggle_optimizer(optimizer_d)  # 激活判别器优化器
+        valid = torch.ones(x.size(0), 1).type_as(x)  # 创建真标签，用于判别器
+        real_loss = self.adversarial_loss(
+            self.DNet(y), valid
+        )  # 计算真实图像的判别器损失
+        fake = torch.zeros(x.size(0), 1).type_as(x)  # 创建假标签，用于判别器
+        fake_loss = self.adversarial_loss(
+            self.DNet(pred.detach()), fake
+        )  # 计算生成图像的判别器损失
+        d_loss = (
+            self.scheduler.get_last_lr()[0] * (real_loss + fake_loss) / 2
+        )  # 总判别器损失
+        self.manual_backward(d_loss)  # 手动计算梯度
+        optimizer_d.step()  # 更新判别器参数
+        optimizer_d.zero_grad()  # 清空判别器梯度
+        self.untoggle_optimizer(optimizer_d)  # 关闭判别器优化器
+
+        # 计算评估指标
+        psnr = tm.functional.image.peak_signal_noise_ratio(pred, y)  # 计算峰值信噪比
+        ssim = tm.functional.image.structural_similarity_index_measure(
+            pred, y
+        )  # 计算结构相似性
+        self.log("g_loss", g_loss)  # 记录生成器对抗损失
+        self.log("d_loss", d_loss)  # 记录判别器损失
+        self.log("train_l1loss", l1loss)  # 记录训练L1损失
+        self.log("train_psnr", psnr)  # 记录训练PSNR
+        self.log("train_ssim", ssim)  # 记录训练SSIM
         self.log("learning_rate", self.scheduler.get_last_lr()[0])  # 记录学习率
-        return loss
 
     def validation_step(self, batch, batch_idx):
         """
         单个验证步骤的逻辑。
+        采用分块的方式进行推理。
 
         Args:
             batch: 一个批次的验证数据。
             batch_idx: 批次的索引。
         """
-        image, label = (batch["image"], batch["label"])
-        prediction = self(image)
-        ce_loss = self.ce_loss(prediction, label)  # 计算交叉熵损失
-        loss = ce_loss  # 总损失等于交叉熵损失
-        self.valid_pred.append(prediction)  # 存储预测值
-        self.valid_label.append(label)  # 存储真实标签
-        self.log("valid_ce_loss", ce_loss)  # 记录验证集交叉熵损失
-        self.log("valid_loss", loss)  # 记录验证集总损失
+        x, y = batch  # x 是低分辨率图像，y 是高分辨率图像
+        b, c, h, w = x.shape  # 获取输入图像的形状
+        size = self.opt.image_size  # 获取图像块的大小
+        pred = torch.ones(
+            (b, c, h, w), device=x.device
+        )  # 创建一个与输入图像大小相同的全1张量，用于存储预测结果
+        m, n = h // size, w // size  # 计算输入图像可以分割成多少个图像块
+        for i in range(m):
+            for j in range(n):
+                patch = x[
+                    :, :, i * size : (i + 1) * size, j * size : (j + 1) * size
+                ]  # 取出当前图像块
+                patch = self.model(patch)  # 将图像块输入到生成器模型中
+                pred[:, :, i * size : (i + 1) * size, j * size : (j + 1) * size] = (
+                    patch  # 将生成器模型的输出放到预测结果张量中
+                )
+        pred = torch.clamp(pred, -1, 1)  # 将像素值限制在 -1 到 1 之间
+        l1loss = self.l1loss(pred, y)  # 计算验证集上的 L1 损失
+        psnr = tm.functional.image.peak_signal_noise_ratio(
+            pred, y
+        )  # 计算验证集上的 PSNR
+        ssim = tm.functional.image.structural_similarity_index_measure(
+            pred, y
+        )  # 计算验证集上的 SSIM
+        self.log("valid_psnr", psnr)  # 记录验证集上的 PSNR
+        self.log("valid_ssim", ssim)  # 记录验证集上的 SSIM
+        self.log(
+            "valid_l1loss", l1loss, prog_bar=True
+        )  # 记录验证集上的 L1 损失，并显示在进度条上
 
     def on_train_epoch_end(self):
         """
-        在每个训练周期结束时计算并记录训练指标。
+        在每个训练周期结束时调用。
+        目前为空，如有需要可以添加操作。
         """
-        self.train_pred = torch.cat(self.train_pred, 0)  # 将所有批次的预测值拼接在一起
-        self.train_label = torch.cat(
-            self.train_label, 0
-        )  # 将所有批次的真实标签拼接在一起
-        preds = torch.argmax(self.train_pred, dim=1)  # 将预测值转换为类别索引
-
-        iou = self.train_iou(preds, self.train_label)  # 计算IOU指标
-        dice = self.train_dice(preds, self.train_label)  # 计算Dice指标
-        for i in range(self.opt.num_classes):  # 记录每个类别的指标
-            self.log(f"iou/train_iou_{i}", iou[i])
-            self.log(f"dice/train_dice_{i}", dice[i])
-        self.log("iou/train_iou", iou.mean())  # 记录平均IOU
-        self.log("dice/train_dice", dice.mean())  # 记录平均Dice
-        # 清空存储预测值和标签的列表，为下一个训练周期做准备
-        self.train_pred = []
-        self.train_label = []
+        pass
 
     def on_validation_epoch_end(self):
         """
-        在每个验证周期结束时计算并记录验证指标。
+        在每个验证周期结束时调用。
+        目前为空，如有需要可以添加操作。
         """
-        self.valid_pred = torch.cat(self.valid_pred, 0)  # 将所有批次的预测值拼接在一起
-        self.valid_label = torch.cat(
-            self.valid_label, 0
-        )  # 将所有批次的真实标签拼接在一起
-        preds = torch.argmax(self.valid_pred, dim=1)  # 将预测值转换为类别索引
-
-        iou = self.valid_iou(preds, self.valid_label)  # 计算IOU指标
-        dice = self.valid_dice(preds, self.valid_label)  # 计算Dice指标
-
-        for i in range(self.opt.num_classes):  # 记录每个类别的指标
-            self.log(f"iou/valid_iou_{i}", iou[i])
-            self.log(f"dice/valid_dice_{i}", dice[i])
-        self.log("iou/valid_iou", iou.mean())  # 记录平均IOU
-        self.log("dice/valid_dice", dice.mean())  # 记录平均Dice
-        # 清空存储预测值和标签的列表，为下一个验证周期做准备
-        self.valid_pred = []
-        self.valid_label = []
+        pass
